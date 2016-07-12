84fc7e077c2e923d1498656bbb5e877b3eeea94b
yiwen song <songzgy@gmail.com>
2016-07-12 01:53:20 -0700

    tictactoe for testing mcts

--- montecarlo.py
+++ montecarlo.py
@@ -68,3 +68,68 @@ def mcts(game,max_sim=1000):
   best = points.index(max_points)
 
   return best
+
+
+
+class TicTacToe():
+  def __init__(self,arg=None):
+    if arg is not None:
+      self.board = arg.board
+      self.turn = arg.turn
+      return
+    self.board = [0] * 9
+    self.turn = 0
+
+  def active(self):
+    return self.turn
+
+  def moves(self):
+    mv = []
+    for i in xrange(9):
+      if self.board[i] == 0:
+        mv.append(i)
+    return mv
+
+  def copy(self):
+    return TicTacToe(arg=self)
+
+  def move(self,move):
+    if self.board[move] != 0:
+      return
+    self.board[move] = int((self.turn - .5) * 2)
+    self.turn = (self.turn + 1) % 2
+
+  def payoff(self):
+    b = self.board
+    for i in xrange(3):
+      if b[3*i] == b[3*i+1] == b[3*i+2]:
+        return b[3*i]
+      if b[i] == b[i+3] == b[i+6]:
+        return b[i]
+    if b[0] == b[4] == b[8]:
+      return b[0]
+    if b[2] == b[4] == b[6]:
+      return b[2]
+    return 0
+
+  def __str__(self):
+    s = ''
+    for i in xrange(9):
+      if self.board[i] == -1:
+        s += 'x '
+      elif self.board[i] == 1:
+        s += 'o '
+      else:
+        s += '  '
+      if i % 3 == 2:
+        s += '\n'
+    return s
+
+
+t = TicTacToe()
+order = range(9)
+random.shuffle(order)
+for i in order:
+  t.move(i)
+  print t.moves()
+print t
