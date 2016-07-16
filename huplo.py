100c28146e8e9caf691045d49446cdb959894172
yiwen song <songzgy@gmail.com>
2016-07-16 02:58:56 -0700

    skeleton for huplo game

---
+++ huplo.py
@@ -0,0 +1,27 @@
+import montecarlo
+import game
+
+class HUPLO_game():
+  '''This is the class that will hook up the MCTS code with the game code'''
+
+  def __init__(self,arg=None):
+    pass
+  
+  def __str__(self):
+    s = ''
+    return s 
+
+  def active(self):
+    return 0
+
+  def copy(self):
+    return HUPLO(self)
+
+  def moves(self):
+    return []
+
+  def move(self,mv):
+    pass
+
+  def payoff(self):
+    return [0,0]
